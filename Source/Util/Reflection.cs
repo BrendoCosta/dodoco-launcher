a97825ebddf8b5e22e03be293a260fdf8b8ab954
Brendo Costa <brendocosta@id.uff.br>
2023-05-08 14:45:36 -0300

    Added util class with reflection-related methods

---
+++ Source/Util/Reflection.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+
+namespace Dodoco.Util {
+
+    public static class Reflection {
+
+        public static string GetCurrentMethod() => GetMethod(2);
+        public static string GetCallingMethod() => GetMethod(3);
+
+        public static string GetMethod(int stack) {
+
+            MethodBase? methodInfo = new System.Diagnostics.StackTrace().GetFrame(stack)?.GetMethod();
+
+            string className = "UnknownClass";
+            string methodName = "UnknownMethod";
+
+            if (methodInfo != null) {
+
+                methodName = methodInfo.IsConstructor ? "Constructor" : methodInfo.Name;
+                
+                if (methodInfo.ReflectedType?.FullName != null) {
+
+                    className = methodInfo.ReflectedType.FullName;
+
+                } else if (methodInfo.ReflectedType != null) {
+
+                    className = methodInfo.ReflectedType.Name;
+
+                }
+
+            }
+
+            return $"{className}.{methodName}";
+
+        }
+
+    }
+
+}
