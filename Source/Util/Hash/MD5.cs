b8057275a8034699adad2d0cd227bcd8de3bbaa4
Brendo Costa <brendocosta@id.uff.br>
2023-06-01 13:15:20 -0300

    Added file hashing

--- Source/Util/Hash/MD5.cs
+++ Source/Util/Hash/MD5.cs
@@ -6,17 +6,40 @@ namespace Dodoco.Util.Hash {
 
         public static string ComputeHash(string buffer) {
 
-            System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create();
-            byte[] hash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(buffer));
-            return System.Convert.ToHexString(hash);
+            using (System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create()) {
+
+                byte[] hash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(buffer));
+                return System.Convert.ToHexString(hash);
+
+            }
 
         }
 
         public static string ComputeHash(byte[] buffer) {
 
-            System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create();
-            byte[] hash = md5Hash.ComputeHash(buffer);
-            return System.Convert.ToHexString(hash);
+            using (System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create()) {
+
+                byte[] hash = md5Hash.ComputeHash(buffer);
+                return System.Convert.ToHexString(hash);
+
+            }
+
+        }
+
+        public static string ComputeHash(FileInfo file) {
+
+            if (!file.Exists) throw new Exception($"The given file doesn't exist");
+
+            using (System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create()) {
+
+                using (FileStream stream = file.OpenRead()) {
+
+                    byte[] hash = md5Hash.ComputeHash(stream);
+                    return System.Convert.ToHexString(hash);
+
+                }
+
+            }
 
         }
 
