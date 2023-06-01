4552c21da4bd7cffe8460af41aa499b80b5ea426
Brendo Costa <brendocosta@id.uff.br>
2023-06-01 12:46:48 -0300

    Added Twind and Tailwind

---
+++ Web/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+import { DodocoConfig } from "./dodoco-launcher.config.js";
+import * as Path from "path";
+import colors from "tailwindcss/colors";
+
+export default {
+    content: [
+        Path.join(__dirname, DodocoConfig.paths.source, "**/*.{html,js,svelte,ts}"),
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
+    ],
+    darkMode: "class",
+    theme: {
+        colors: Object.assign(colors, {
+            "darkgray": "#393b40",
+            "lightgray": "#78797b",
+            "golden": "#dcbc60",
+        })
+    }
+}
+
