1d44e75c51b73de870b0f64753a1ead39f064d83
Brendo Costa <brendocosta@id.uff.br>
2023-06-07 23:12:08 -0300

    Shows progress bar and message when performing operations

--- Web/Source/Pages/Launcher.svelte
+++ Web/Source/Pages/Launcher.svelte
@@ -1,33 +1,50 @@
 <script lang="ts">
 
-    import { GameInstance, LauncherInstance, i18nInstance } from "@Dodoco/GlobalInstances";
-    import { Language, LanguageConstants, LanguageName } from "@Dodoco/Language";
-    import { Button, ButtonGroup, Checkbox, LoadingDots, Modal, ModalControl, ProgressBar, ScoopedFrame } from "@Dodoco/Components";
+    import { Nullable } from "@Dodoco/index";
+    import { GameInstance, i18nInstance } from "@Dodoco/GlobalInstances";
+    import { Button, ButtonGroup, ModalControl, ProgressBar } from "@Dodoco/Components";
     import Settings from "./Settings.svelte";
-
+    
+    import { onMount } from "svelte";
     import Icon from "@iconify/svelte";
-    import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@rgossiaux/svelte-headlessui";
-    import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from "@rgossiaux/svelte-headlessui";
     
     // Generated types
     import { LauncherController } from "@Dodoco/Generated/Dodoco/Network/Controller/LauncherController";
     import { GameState } from "@Dodoco/Generated/Dodoco/Game/GameState";
+    import { ApplicationProgressReport } from "@Dodoco/Generated/Dodoco/Application/ApplicationProgressReport";
 
     $: game = $GameInstance;
     $: _i18n = $i18nInstance;
     let settingsModal: ModalControl;
-    let progressBarText: string = "";
-    let progressBarValue: number = 0;
-    let mycheck: boolean = true;
-
-    let selectedLanguage = $i18nInstance.language as Language;
+    let checkIntegrityProgressReport: Nullable<ApplicationProgressReport> = null;
+    let busyOperationProgress: number = 0;
 
     $: IsBusy = (): boolean => {
         return game.State == GameState.CHECKING_INTEGRITY
             || game.State == GameState.DOWNLOADING
             || game.State == GameState.RUNNING
+            || game.State == GameState.REPAIRING_FILES
             || game.State == GameState.UPDATING;
     }
+
+    onMount(() => {
+
+        setInterval(async () => {
+
+            if (IsBusy()) {
+
+                if (game.State == GameState.CHECKING_INTEGRITY) {
+
+                    checkIntegrityProgressReport = await LauncherController.GetInstance().GetLastGameCheckIntegrityProgressReport();
+                    busyOperationProgress = checkIntegrityProgressReport.completionPercentage;
+
+                }
+
+            }
+
+        }, 500);
+
+    });
 
     let selectedTableHelper = (a: any) => a.selected ? "my-custom-class active" : "my-custom-class";
 
@@ -40,11 +57,17 @@
                     {#if IsBusy() }
                         <small class="text-white animate-pulse">
                             {#if game.State == GameState.CHECKING_INTEGRITY }
-                                Checking game's files integrity
+                                {#if checkIntegrityProgressReport != null }
+                                    Checking game's files integrity ({Math.floor(checkIntegrityProgressReport.completionPercentage)}% / ETA: {checkIntegrityProgressReport.estimatedRemainingTime.toString().split(":").map(e => parseInt(e).toString().padStart(2, "0")).join(":")})
+                                {/if}
+                            {:else if game.State == GameState.REPAIRING_FILES }
+                                Repairing game's files
+                            {:else}
+                                Working...
                             {/if}
                         </small>
+                        <ProgressBar value={busyOperationProgress} width="1/4"/>
                     {/if}
-                    <ProgressBar show={IsBusy()} value={0} width="1/4"/>
                     <ButtonGroup>
                         <Button on:click={() => settingsModal.Open() } disabled={IsBusy()}><Icon icon="material-symbols:settings"/>&nbsp;Settings</Button>
                         <Button focused disabled={IsBusy()}>
@@ -64,9 +87,7 @@
             </div>
         </div>
     </div>
-    {#await LauncherController.GetInstance().GetLauncherSettings() then settings }
-        <Settings bind:Root={settingsModal} settings={settings}/>
-    {/await}
+    <Settings bind:Root={settingsModal}/>
 </div>
 <style>
 </style>
