c7af868d1d8fc0675bc40c818afaacced4c669e7
Brendo Costa <brendocosta@id.uff.br>
2023-07-26 12:49:59 -0300

    Fixed GetAvaliableStorageSpace to enable checking directories that don't exist

--- Source/Core/Util/FileSystem/FileSystem.cs
+++ Source/Core/Util/FileSystem/FileSystem.cs
@@ -6,9 +6,24 @@ namespace Dodoco.Core.Util.FileSystem {
 
         public static long GetAvaliableStorageSpace(string directory) {
 
+            /*
+             * If the directory actually doesn't exist, the
+             * "new DriveInfo(directory)" will fail, so we
+             * create the directory, get the free space avaliable
+             * and then delete it.
+            */
+
+            bool directoryExists = Directory.Exists(directory);
+            if (!directoryExists)
+                Directory.CreateDirectory(directory);
+
             DriveInfo drive = new DriveInfo(directory);
-            long storageFreeBytes = drive.AvailableFreeSpace; 
+            long storageFreeBytes = drive.TotalFreeSpace; 
             Logger.GetInstance().Log($"There is {DataUnitFormatter.Format(storageFreeBytes)} of space available in the drive {drive.VolumeLabel}");
+            
+            if (!directoryExists)
+                Directory.Delete(directory);
+            
             return storageFreeBytes;
 
         }
