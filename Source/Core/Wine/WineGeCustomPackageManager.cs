27175c24bcaebc3f4984b48bdd44ff0da38d5268
Brendo Costa <brendocosta@id.uff.br>
2023-07-26 12:59:21 -0300

    Implements interface changes; fixed regex patterns to support two digits version numbers

--- Source/Core/Wine/WineGeCustomPackageManager.cs
+++ Source/Core/Wine/WineGeCustomPackageManager.cs
@@ -14,15 +14,19 @@ namespace Dodoco.Core.Wine {
 
     public class WineGeCustomPackageManager: IWinePackageManager {
 
+        public WinePackageManagerState State { get; private set; } = WinePackageManagerState.READY;
         public string PackagesRootDirectory { get; private set; }
 
-        private const string RELEASE_TAG_NAME_PATTERN = @"GE-Proton[\d]-[\d]";
-        private const string RELEASE_DIRECTORY_PATTERN = @"lutris-GE-Proton[\d]-[\d]-x86_64";
-        private const string RELEASE_FILENAME_PATTERN = @"wine-lutris-GE-Proton[\d]-[\d]-x86_64.tar.xz";
-        private const string RELEASE_CHECKSUM_FILENAME_PATTERN = @"wine-lutris-GE-Proton[\d]-[\d]-x86_64.sha512sum";
+        private const string RELEASE_TAG_NAME_PATTERN = @"GE-Proton[\d]+-[\d]+";
+        private const string RELEASE_DIRECTORY_PATTERN = @"lutris-GE-Proton[\d]+-[\d]+-x86_64";
+        private const string RELEASE_FILENAME_PATTERN = @"wine-lutris-GE-Proton[\d]+-[\d]+-x86_64.tar.xz";
+        private const string RELEASE_CHECKSUM_FILENAME_PATTERN = @"wine-lutris-GE-Proton[\d]+-[\d]+-x86_64.sha512sum";
         
         private GitHubReposApiFactory apiFactory;
         private Cache<List<Release>> avaliableReleasesCache = new Cache<List<Release>>(new List<Release>());
+
+        public event EventHandler<WinePackageManagerState> OnStateUpdate = delegate {};
+        public event EventHandler AfterReleaseDownload = delegate {};
 
         public WineGeCustomPackageManager(string packagesRootDirectory, GitHubReposApiFactory apiFactory) {
 
@@ -31,94 +35,111 @@ namespace Dodoco.Core.Wine {
 
         }
 
-        public async Task InstallPackageFromRelease(Release release, ProgressReporter<ProgressReport> progress) {
+        public async Task InstallPackageFromRelease(Release release, ProgressReporter<ProgressReport>? progress = null) {
 
-            Logger.GetInstance().Log($"Installing the release \"{release.tag_name}\"...");
-
-            Asset tarballAsset = release.assets.Find(x => Regex.IsMatch(x.name, WineGeCustomPackageManager.RELEASE_FILENAME_PATTERN));
-            Asset checksumAsset = release.assets.Find(x => Regex.IsMatch(x.name, WineGeCustomPackageManager.RELEASE_CHECKSUM_FILENAME_PATTERN));
-
-            if (!release.assets.Contains(tarballAsset) || !release.assets.Contains(checksumAsset))
-                throw new WineException($"Unable to find the requested release");
-            
-            long storageFreeBytes = FileSystem.GetAvaliableStorageSpace(this.PackagesRootDirectory);
-            if (storageFreeBytes <= (tarballAsset.size + checksumAsset.size))
-                throw new WineException("There is not enough space available in the storage device for Wine's installation");
-            
-            Logger.GetInstance().Log($"Downloading the release file \"{Path.Join(this.PackagesRootDirectory, tarballAsset.name)}\"...");
-
-            await Client.GetInstance().DownloadFileAsync(new Uri(tarballAsset.browser_download_url), Path.Join(this.PackagesRootDirectory, tarballAsset.name), progress);
-            await Client.GetInstance().DownloadFileAsync(new Uri(checksumAsset.browser_download_url), Path.Join(this.PackagesRootDirectory, checksumAsset.name), progress);
-
-            Logger.GetInstance().Log($"Computing release file's SHA-512 checksum...");
-
-            string tarballChecksum = new Hash(SHA512.Create()).ComputeHash(Path.Join(this.PackagesRootDirectory, tarballAsset.name));
-            string expectedChecksum = (await File.OpenText(Path.Join(this.PackagesRootDirectory, checksumAsset.name)).ReadToEndAsync()).Substring(0, 128).ToUpper();
-
-            if (tarballChecksum == expectedChecksum) {
-
-                Logger.GetInstance().Log($"The downloaded tarball file's SHA-512 checksum match the expected checksum ({expectedChecksum})");
-
-            } else {
-
-                throw new WineException($"The downloaded tarball file's SHA-512 checksum ({tarballChecksum}) doesn't match the expected checksum ({expectedChecksum})");
-
-            }
-
-            Logger.GetInstance().Log($"Decompressing the release file \"{Path.Join(this.PackagesRootDirectory, tarballAsset.name)}\"...");
-
-            Process process = new Process();
-            
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = "/usr/bin/tar";
-            info.WorkingDirectory = this.PackagesRootDirectory;
-            info.Arguments = $"-xvJf {Path.Join(this.PackagesRootDirectory, tarballAsset.name)}";
-            info.RedirectStandardOutput = true;
-            info.RedirectStandardError = true;
-            info.UseShellExecute = false;
-            info.CreateNoWindow = false;
-
-            process.StartInfo = info;
-            
-            process.OutputDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) => {
-                
-                if (!string.IsNullOrWhiteSpace(e.Data))
-                    Logger.GetInstance().Log($"TAR: {e.Data}");
-                
-            });
-
-            process.ErrorDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) => {
-                
-                if (!string.IsNullOrWhiteSpace(e.Data))
-                    throw new WineException($"TAR: {e.Data}");
-                
-            });
+            WinePackageManagerState previousState = this.State;
+            this.UpdateState(WinePackageManagerState.DOWNLOADING_PACKAGE);
 
             try {
 
-                if (process.Start()) {
+                Logger.GetInstance().Log($"Installing the release \"{release.tag_name}\"...");
 
-                    Logger.GetInstance().Log($"Successfully started TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"");
+                Asset tarballAsset = release.assets.Find(x => Regex.IsMatch(x.name, WineGeCustomPackageManager.RELEASE_FILENAME_PATTERN));
+                Asset checksumAsset = release.assets.Find(x => Regex.IsMatch(x.name, WineGeCustomPackageManager.RELEASE_CHECKSUM_FILENAME_PATTERN));
 
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
-                    await process.WaitForExitAsync();
+                if (!release.assets.Contains(tarballAsset))
+                    throw new WineException($"Unable to find the tarball asset for the requested release \"{release.tag_name}\"");
+                
+                if (!release.assets.Contains(checksumAsset))
+                    throw new WineException($"Unable to find the checksum asset for the requested release \"{release.tag_name}\"");
+                
+                long storageFreeBytes = FileSystem.GetAvaliableStorageSpace(this.PackagesRootDirectory);
+                if (storageFreeBytes <= (tarballAsset.size + checksumAsset.size))
+                    throw new WineException("There is not enough space available in the storage device for Wine's installation");
+                
+                Logger.GetInstance().Log($"Downloading the release file \"{Path.Join(this.PackagesRootDirectory, tarballAsset.name)}\"...");
 
-                    Logger.GetInstance().Log($"Successfully finished TAR's process");
+                await Client.GetInstance().DownloadFileAsync(new Uri(tarballAsset.browser_download_url), Path.Join(this.PackagesRootDirectory, tarballAsset.name), progress);
+                await Client.GetInstance().DownloadFileAsync(new Uri(checksumAsset.browser_download_url), Path.Join(this.PackagesRootDirectory, checksumAsset.name), progress);
+
+                this.UpdateState(WinePackageManagerState.CHECKING_PACKAGE_CHECKSUM);
+                Logger.GetInstance().Log($"Computing release file's SHA-512 checksum...");
+
+                string tarballChecksum = new Hash(SHA512.Create()).ComputeHash(Path.Join(this.PackagesRootDirectory, tarballAsset.name));
+                string expectedChecksum = (await File.OpenText(Path.Join(this.PackagesRootDirectory, checksumAsset.name)).ReadToEndAsync()).Substring(0, 128).ToUpper();
+
+                if (tarballChecksum == expectedChecksum) {
+
+                    Logger.GetInstance().Log($"The downloaded tarball file's SHA-512 checksum match the expected checksum ({expectedChecksum})");
 
                 } else {
 
-                    throw new WineException($"Failed to start the TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"");
+                    throw new WineException($"The downloaded tarball file's SHA-512 checksum ({tarballChecksum}) doesn't match the expected checksum ({expectedChecksum})");
 
                 }
 
-            } catch (Exception e) {
+                this.UpdateState(WinePackageManagerState.DECOMPRESSING_PACKAGE);
+                Logger.GetInstance().Log($"Decompressing the release file \"{Path.Join(this.PackagesRootDirectory, tarballAsset.name)}\"...");
 
-                throw new WineException($"Failed to start the TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"", e);
+                Process process = new Process();
+                
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = "/usr/bin/tar";
+                info.WorkingDirectory = this.PackagesRootDirectory;
+                info.Arguments = $"-xvJf {Path.Join(this.PackagesRootDirectory, tarballAsset.name)}";
+                info.RedirectStandardOutput = true;
+                info.RedirectStandardError = true;
+                info.UseShellExecute = false;
+                info.CreateNoWindow = false;
+
+                process.StartInfo = info;
+                
+                process.OutputDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) => {
+                    
+                    if (!string.IsNullOrWhiteSpace(e.Data))
+                        Logger.GetInstance().Log($"TAR: {e.Data}");
+                    
+                });
+
+                process.ErrorDataReceived += new DataReceivedEventHandler((object sender, DataReceivedEventArgs e) => {
+                    
+                    if (!string.IsNullOrWhiteSpace(e.Data))
+                        throw new WineException($"TAR: {e.Data}");
+                    
+                });
+
+                try {
+
+                    if (process.Start()) {
+
+                        Logger.GetInstance().Log($"Successfully started TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"");
+
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        await process.WaitForExitAsync();
+
+                        Logger.GetInstance().Log($"Successfully finished TAR's process");
+
+                    } else {
+
+                        throw new WineException($"Failed to start the TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"");
+
+                    }
+
+                } catch (Exception e) {
+
+                    throw new WineException($"Failed to start the TAR's process inside directory \"{info.WorkingDirectory}\" with the command \"{info.FileName} {info.Arguments}\"", e);
+
+                }
+
+                Logger.GetInstance().Log($"Successfully installed the release \"{release.tag_name}\"");
+                this.AfterReleaseDownload.Invoke(this, EventArgs.Empty);
+
+            } finally {
+
+                this.UpdateState(previousState);
 
             }
-
-            Logger.GetInstance().Log($"Successfully installed the release \"{release.tag_name}\"");
 
             return;
 
@@ -192,6 +213,14 @@ namespace Dodoco.Core.Wine {
         public async Task<Release> GetLatestRelease() {
 
             return (await this.GetAvaliableReleases()).First();
+
+        }
+
+        private void UpdateState(WinePackageManagerState newState) {
+
+            Logger.GetInstance().Debug($"Updating package manager's state from {this.State.ToString()} to {newState.ToString()}");
+            this.State = newState;
+            this.OnStateUpdate.Invoke(this, newState);
 
         }
 
