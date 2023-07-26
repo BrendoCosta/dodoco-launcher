1fd5d1c034386394c1a0d1d8b05d5a8d58469a15
Brendo Costa <brendocosta@id.uff.br>
2023-07-26 12:55:56 -0300

    Added BytesTransferred and TotalBytesTransferred

--- Source/Core/ProgressReport.cs
+++ Source/Core/ProgressReport.cs
@@ -3,6 +3,8 @@ namespace Dodoco.Core {
     public record ProgressReport {
 
         public double BytesPerSecond { get; set; } = 0.0D;
+        public double BytesTransferred { get; set; } = 0.0D;
+        public double TotalBytesTransferred { get; set; } = 0.0D;
         public double CompletionPercentage { get; set; } = 0.0D;
         public TimeSpan EstimatedRemainingTime { get; set; } = TimeSpan.FromSeconds(0.0D);
         public string? Message { get; set; } = null;
