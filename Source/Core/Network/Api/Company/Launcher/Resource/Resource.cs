c73b145908729da72f6105df120eb33773da6e73
Brendo Costa <brendocosta@id.uff.br>
2023-07-26 12:53:23 -0300

    Changed byte sizes from ulong to long and added package_size to Segment

--- Source/Core/Network/Api/Company/Launcher/Resource/Resource.cs
+++ Source/Core/Network/Api/Company/Launcher/Resource/Resource.cs
@@ -38,11 +38,11 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
             public string name { get; set; }
             public string version { get; set; }
             public string path { get; set; }
-            public ulong size { get; set; }
+            public long size { get; set; }
             public string md5 { get; set; }
             public bool is_recommended_update { get; set; }
             public List<VoicePack> voice_packs { get; set; }
-            public ulong package_size { get; set; }
+            public long package_size { get; set; }
             
         }
 
@@ -58,13 +58,13 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
             public string name { get; set; }
             public string version { get; set; }
             public string path { get; set; }
-            public ulong size { get; set; }
+            public long size { get; set; }
             public string md5 { get; set; }
             public string entry { get; set; }
             public List<VoicePack> voice_packs { get; set; }
             public Uri decompressed_path { get; set; }
             public List<Segment> segments { get; set; }
-            public ulong package_size { get; set; }
+            public long package_size { get; set; }
             
         }
 
@@ -80,7 +80,7 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
             public string name { get; set; }
             public string version { get; set; }
             public string path { get; set; }
-            public ulong size { get; set; }
+            public long size { get; set; }
             public string md5 { get; set; }
             public string entry { get; set; }
             
@@ -90,6 +90,7 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
 
             public string path { get; set; }
             public string md5 { get; set; }
+            public long package_size { get; set; }
             
         }
 
@@ -98,9 +99,9 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
             public string language { get; set; }
             public string name { get; set; }
             public string path { get; set; }
-            public ulong size { get; set; }
+            public long size { get; set; }
             public string md5 { get; set; }
-            public ulong package_size { get; set; }
+            public long package_size { get; set; }
             
         }
 
