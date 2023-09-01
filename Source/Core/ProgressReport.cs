afdfc0f5bc2c6452b890ffc6154d9a33ef40169a
Brendo Costa <brendocosta@id.uff.br>
2023-09-01 20:32:51 -0300

    Simplified fields

--- Source/Core/ProgressReport.cs
+++ Source/Core/ProgressReport.cs
@@ -2,11 +2,10 @@ namespace Dodoco.Core {
 
     public record ProgressReport {
 
-        public double BytesPerSecond { get; set; } = 0.0D;
-        public double BytesTransferred { get; set; } = 0.0D;
-        public double TotalBytesTransferred { get; set; } = 0.0D;
-        public double CompletionPercentage { get; set; } = 0.0D;
-        public TimeSpan EstimatedRemainingTime { get; set; } = TimeSpan.FromSeconds(0.0D);
+        public double Done { get; set; } = 0.0D;
+        public double Total { get; set; } = 0.0D;
+        public double? Rate { get; set; } = null;
+        public TimeSpan? EstimatedRemainingTime { get; set; } = null;
         public string? Message { get; set; } = null;
 
     }
