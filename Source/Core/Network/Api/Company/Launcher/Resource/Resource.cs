6a8ee4fb54b8347ec30246d60dd66c9dc1f0184e
Brendo Costa <brendocosta@id.uff.br>
2023-09-01 20:42:01 -0300

    Added Game type to pre_download_game

--- Source/Core/Network/Api/Company/Launcher/Resource/Resource.cs
+++ Source/Core/Network/Api/Company/Launcher/Resource/Resource.cs
@@ -12,7 +12,7 @@ namespace Dodoco.Core.Network.Api.Company.Launcher.Resource {
             public Plugin plugin { get; set; }
             public string web_url { get; set; }
             public object force_update { get; set; }
-            public object pre_download_game { get; set; }
+            public Game? pre_download_game { get; set; }
             public List<DeprecatedPackage> deprecated_packages { get; set; }
             public object sdk { get; set; }
             public List<DeprecatedFile> deprecated_files { get; set; }
