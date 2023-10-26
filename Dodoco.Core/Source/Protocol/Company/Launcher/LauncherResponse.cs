5b92f1f992d83ec22ae2538b12c72d5295112c89
Brendo Costa <brendocosta@id.uff.br>
2023-10-26 17:47:55 -0300

    Added EnsureSuccessStatusCode method

--- Dodoco.Core/Source/Protocol/Company/Launcher/LauncherResponse.cs
+++ Dodoco.Core/Source/Protocol/Company/Launcher/LauncherResponse.cs
@@ -11,4 +11,19 @@ public abstract class LauncherResponse {
 
     }
 
+    /// <summary>
+    /// Assert the object contains a successful response code from the server.
+    /// Throws a <see cref="T:Dodoco.Core.Protocol.ProtocolException" /> if the
+    /// response code received from the server is not a <see cref="F:Dodoco.Core.Protocol.Company.Launcher.LauncherResponseStatus.SUCCESS" />.
+    /// </summary>
+    /// <exception cref="T:Dodoco.Core.Protocol.ProtocolException">
+    ///     The response code received from the server is not a <see cref="F:Dodoco.Core.Protocol.Company.Launcher.LauncherResponseStatus.SUCCESS" />.
+    /// </exception>
+    public void EnsureSuccessStatusCode() {
+
+        if (!this.IsSuccessfull())
+            throw new ProtocolException("Received an unsuccessful response code from the server");
+
+    }
+
 }
