95cbefc64c7d44a5c2ef1f15a18b5d654b983d7a
Brendo Costa <brendocosta@id.uff.br>
2023-10-29 22:56:28 -0300

    Added GameInstallationManager

---
+++ Dodoco.Core/Source/Game/GameInstallationManagerState.cs
@@ -0,0 +1,10 @@
+namespace Dodoco.Core.Game;
+
+public enum GameInstallationManagerState {
+
+    DOWNLOADED,
+    RECOVERING_DOWNLOADED_SEGMENTS,
+    DOWNLOADING_SEGMENTS,
+    UNZIPPING_SEGMENTS
+
+}
