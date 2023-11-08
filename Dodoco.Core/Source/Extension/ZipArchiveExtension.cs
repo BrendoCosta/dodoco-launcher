2024f8124ee002a63298db03a6e7fe863e542260
Brendo Costa <brendocosta@id.uff.br>
2023-11-08 00:01:30 -0300

    Bugfix - entry directory not being created

--- Dodoco.Core/Source/Extension/ZipArchiveExtension.cs
+++ Dodoco.Core/Source/Extension/ZipArchiveExtension.cs
@@ -15,20 +15,17 @@ namespace Dodoco.Core.Extension {
             for (int i = 0; i < archive.Entries.Count; i++) {
 
                 ZipArchiveEntry entry = archive.Entries[i];
+
+                /* The ZipArchiveEntry.ExtractToFile method doesn't creates zip archive's internal
+                 * directories due some unknown reason, so we need to create them otherwise an
+                 * UnauthorizedAccessException will be raised up.
+                */
+
+                string? entryFullDir = Path.GetDirectoryName(entry.FullName);
+                if (entryFullDir != null && !string.IsNullOrEmpty(entryFullDir))
+                    Directory.CreateDirectory(Path.Join(directory, entryFullDir));
+
                 string path = Path.Join(directory, entry.FullName);
-
-                if (entry.FullName.EndsWith("/") && string.IsNullOrWhiteSpace(entry.Name)) {
-
-                    /* The ZipArchiveEntry.ExtractToFile method doesn't creates zip archive's internal
-                     * directories due some unknown reason, so we need to create them otherwise an
-                     * UnauthorizedAccessException will be raised up.
-                    */
-
-                    Directory.CreateDirectory(path);
-                    continue;
-
-                }
-
                 entry.ExtractToFile(path, overwrite);
 
                 ProgressReport report = new ProgressReport {
